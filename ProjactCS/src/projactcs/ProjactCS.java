f659ee89b89ef3888d9a715c0c052e6aaf0362b0
fatimahaljohar <110064104+fatimahaljohar@users.noreply.github.com>
2022-09-08 00:12:28 +0300

    Add files via upload

---
+++ ProjactCS/src/projactcs/ProjactCS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projactcs;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author user
+ */
+public class ProjactCS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyFrame frm = new MyFrame();
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frm.setSize(858,523);
+        frm.setVisible(true);
+    }
+    
+}
